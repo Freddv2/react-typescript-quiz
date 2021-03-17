cbb8e818f64150caa33d22b0f3d676ede7ece8ac
fvincent <fvincent@okiok.com>
2021-03-16 22:08:18 -0400

    WIP implemeting questions logic.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function App() {
-  return <div className="App">Quiz</div>;
+// Components
+import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTIONS = 20
+
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [questionNumber, setQuestionNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
+  return (
+      <div className="App">
+        <h1>Satisfaite, du Jeu des Devinettes</h1>
+          <button className="start" onClick={startTrivia}>
+            Débuter
+          </button>
+          <p className="score">Score:</p>
+          <p>Chargement...</p>
+          <QuestionCard
+            questionNb={questionNumber + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[questionNumber].question}
+            answers={questions[questionNumber].answers}
+            userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
+            callback={checkAnswer}
+          />
+          <button className="next" onClick={nextQuestion}>
+            Prochaine question
+          </button>
+      </div>
+  );
 }
 
 export default App;
